6fc5946d7e962b5f4b20a9de3b7b2e71e3e90be6
Isaac-Zegarra123 <isaacrodrigozegarramamani@gmail.com>
2026-03-25 21:22:49 -0400

    correccion de fronented

--- fronted/src/services/api.js
+++ fronted/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.VITE_URL_BACKEND || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_URL_BACKEND || "http://localhost:5000",
 });
 
 export default API;
