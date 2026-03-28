8b707274fe1c3b0a740b7c05a4e9016e357e32c2
Isaac-Zegarra123 <isaacrodrigozegarramamani@gmail.com>
2026-03-27 20:16:09 -0400

    fix routes api

--- fronted/src/services/api.js
+++ fronted/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.APP_URL_BACKEND, // || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_URL_BACKEND || "http://localhost:5000",
 });
 
 export default API;
