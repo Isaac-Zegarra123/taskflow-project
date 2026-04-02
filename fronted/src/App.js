bd41579a890487bfd6a20008a8a1e3cc1fadfb8b
Isaac-Zegarra123 <isaacrodrigozegarramamani@gmail.com>
2026-04-01 21:28:35 -0400

    corriendo el app

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import { lazy, Suspense } from "react";
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 function App() {
   return (
-    <div>
-      <Login />
-      <Register />
-      <Dashboard />
-    </div>
+    <Router>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
